70562ad6fc2e89aaa079843e4031c8d0d21c9d72
hamzaerengencalioglu <hamzaerengencalioglu@gmail.com>
2022-12-09 12:59:03 +0300

    ce103-hw3-library-app added

---
+++ ce103-hw3-library-app/LoginMenu.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace libraryapplication
+{
+    class LoginMenu
+    {
+        public void Loginscreen()
+
+        {
+            Console.Clear();
+            string welcome = @"
+                                 __          ________ _      _____ ____  __  __ ______ 
+                                 \ \        / /  ____| |    / ____/ __ \|  \/  |  ____|
+                                  \ \  /\  / /| |__  | |   | |   | |  | | \  / | |__   
+                                   \ \/  \/ / |  __| | |   | |   | |  | | |\/| |  __|  
+                                    \  /\  /  | |____| |___| |___| |__| | |  | | |____ 
+                                     \/  \/   |______|______\_____\____/|_|  |_|______|
+                                                       
+                                                       
+";
+
+            Console.WriteLine(welcome);
+            Console.WriteLine("Please Enter Your Username :");
+            string username = Console.ReadLine();
+            Console.WriteLine("Please Enter Your Password :");
+            string password = Console.ReadLine();
+            while (true)
+            {
+                if (username == "Eren" && password == "Eren61")
+                {
+                    Console.Clear();
+                    Console.WriteLine("                                            You Have Successfully Logged In");
+                    Thread.Sleep(900);
+
+                    MainMenu menu = new MainMenu();
+                    menu.Menu();
+
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("                                 Your Username Or Password Is Incorrect. Please Try Again");
+                    Thread.Sleep(2000);
+                    LoginMenu login =  new LoginMenu();
+                    login.Loginscreen();
+                }
+                break;
+            }
+        }
+    }
+}
---
+++ ce103-hw3-library-app/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace libraryapplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LoginMenu login = new LoginMenu();
+            login.Loginscreen();
+        }
+    }
+}
